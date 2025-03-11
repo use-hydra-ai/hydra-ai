abf07c80a07388b211d2c6792c40d2c11ed1605a
Alec Flett <alecf@flett.org>
2025-03-11 15:54:04 -0700

    chore: remove excess eslint stuff from showcase

--- showcase/eslint.config.mjs
+++ showcase/eslint.config.mjs
@@ -1,21 +1,12 @@
 import nextJsConfig from "@tambo-ai/eslint-config/next-js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  ...nextJsConfig,
-  { ignores: [".source/"] },
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: [],
-        },
+export default tseslint.config(...nextJsConfig, {
+  languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: [],
       },
     },
   },
-);
+});
