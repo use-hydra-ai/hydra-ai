edb7d52a00dcab2c1be7015a87bac185aca0b0d0
Michael Magan <magan@usehydra.ai>
2025-03-11 17:42:08 -0700

    remove the old hydra stuff :)

--- examples/charts-demo/src/app/model/transaction.ts
+++
@@ -1,7 +0,0 @@
-export interface Transaction {
-  id: number;
-  dollarAmount: number;
-  dateIso: string;
-  category: string;
-  type: "income" | "expense";
-}
--- hydra-server/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HydraBackend } from "./hydra-ai/hydra-ai-backend";
-export * from "./hydra-ai/model";
